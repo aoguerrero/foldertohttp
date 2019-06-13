c0a21b27ae6f7318a028108135f3d152ddb1bf04
Andres Guerrero <andres.guerrero@certicamara.com>
2019-06-13 17:02:24 -0500

    Sitio Angular

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular
+  .module('foldertohttpApp', ['ui.router'])
+  .config([$stateProvider,
+    function ($stateProvider) {
+    }]).run(['$rootScope', '$http', '$location', function ($rootScope, $http, $location) {
+    }]);
+
