3b65b44a4e35a47c335e690c00eb54e7e12e53f6
Andres Guerrero <andres.guerrero@certicamara.com>
2019-06-17 13:58:41 -0500

    Folder browsing

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -3,14 +3,20 @@
 var app = angular.module("foldertohttpApp", []);
 app.controller("mainController", ["$scope", "$http", function ($scope, $http) {
 
+  $scope.token = "";
+
   $scope.goList = false;
 
+  var currentPath = [];
+
+  /* ******************************************************************************** */
+
   $scope.loginData = {
-    username: "",
-    password: ""
+    username: "user",
+    password: "secret1"
   }
 
-  $scope.token = "";
+  /* ******************************************************************************** */
 
   $scope.login = function () {
     $http({
@@ -18,19 +24,63 @@ app.controller("mainController", ["$scope", "$http", function ($scope, $http) {
       url: "http://localhost:8080/login",
       data: $scope.loginData
     }).then(function (response) {
-      console.log(response);
+      $scope.token = response.data.token;
+      $scope.goList = true;
+      $scope.list();
     }, function (error) {
-      console.log(error);
+
     });
   }
 
-  $scope.list = function (path, token) {
+  $scope.list = function (path = "") {
+    if (path != "")
+      currentPath.push(path);
+    $http({
+      method: "GET",
+      url: "http://localhost:8080/list" + $scope.printCurrentPath(),
+      headers: {
+        "token": $scope.token
+      }
+    }).then(function (response) {
+      $scope.dirs = [];
+      $scope.files = [];
+      var items = response.data;
+      items.map(function (item) {
+        if (item.type == 'D') {
+          $scope.dirs.push({ name: item.name });
+        } else {
+          $scope.files.push({ name: item.name });
+        }
+      })
+
+    }, function (error) {
+
+    });
+  }
+
+  $scope.back = function () {
+    if (currentPath.length > 0) {
+      currentPath.pop();
+    }
+    $scope.list();
   }
 
   $scope.download = function (path) {
   }
 
   $scope.logout = function () {
+  }
+
+  /* ******************************************************************************** */
+
+  $scope.printCurrentPath = function () {
+    var result = "";
+    if (currentPath) {
+      for (var i = 0; i < currentPath.length; i++) {
+        result += "/" + currentPath[i];
+      }
+    }
+    return result;
   }
 
 }])
